48c907423931f6a39d6c8f50aab3c1463614fd3c
Cucu Andreea <andreeacucu@hotmail.com>
2018-09-05 22:34:30 +0300

    Cucu test

--- xazcore/Controllers/HomeController.cs
+++ xazcore/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace xazcore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page. CUCU";
 
             return View();
         }
