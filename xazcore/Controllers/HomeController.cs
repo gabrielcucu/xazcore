83a31601094e4de0a29377e5d0746f76f48c2740
Cucu Andreea <andreeacucu@hotmail.com>
2018-09-05 23:14:08 +0300

    Another change

--- xazcore/Controllers/HomeController.cs
+++ xazcore/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace xazcore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page. CUCU Gabriel";
+            ViewData["Message"] = "Your application description page. Vermorel";
 
             return View();
         }
