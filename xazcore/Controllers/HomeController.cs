2b461e54bf5041043cc3a8d3a2b7058f9bae2463
Cucu Andreea <andreeacucu@hotmail.com>
2018-09-08 19:10:46 +0300

    nreew

--- xazcore/Controllers/HomeController.cs
+++ xazcore/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace xazcore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page. Vermorel CUCUCUCUC";
+            ViewData["Message"] = "Your application description page. Vermorel";
 
             return View();
         }
